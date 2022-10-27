51b5236317c42f9f395211ae995c9d61d5a605ec
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-10-27 11:05:17 +0200

    Add ProductDto.Detail

--- Shared/Products/ProductDto.cs
+++ Shared/Products/ProductDto.cs
@@ -8,5 +8,11 @@ public static class ProductDto
         public string Name { get; set; }
         public decimal Price { get; set; }
     }
+
+    public class Detail : Index
+    {
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
 }
 
